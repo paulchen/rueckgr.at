7672ecb6dd53a32896240d968fa1b8a1392d8743
Paul Staroch <paulchen@rueckgr.at>
2021-07-29 17:31:03 +0200

    update index page

--- templates/index_en.php
+++ templates/index_en.php
@@ -9,17 +9,18 @@
 		<li><a href="//torstatus.rueckgr.at">torstatus.rueckgr.at</a> &ndash; a TorStatus page</li>
 		<li><a href="//mail.rueckgr.at">mail.rueckgr.at</a> &ndash; a mail server</li>
 		<!-- <li><a href="//pad.rueckgr.at">pad.rueckgr.at</a> &ndash; an installation of <a href="http://etherpad.org/">Etherpad</a></li> -->
-		<li><a href="//apeiron.rueckgr.at">apeiron.rueckgr.at</a> &ndash; notes from Math lectures at <a href="http://www.tuwien.ac.at/en/tuwien_home/">Vienna University of Technology</a></li>
+		<li><a href="//apeiron.rueckgr.at">apeiron.rueckgr.at</a> &ndash; notes from Math lectures at <a href="http://www.tuwien.ac.at/en/tuwien_home/">TU Wien</a></li>
 		<li><a href="//punkte.huntu.at">punkte.huntu.at</a> &ndash; the grading tool of <a href="http://huntu.at">hunTU</a></li>
 		<li><a href="//ntp.rueckgr.at">ntp.rueckgr.at</a> &ndash; an <a href="http://www.ntp.org/">NTP</a> server free to use for everyone that participates in the <a href="http://www.pool.ntp.org">NTP pool project</a></li>
 		<li><a href="/cb">rueckgr.at/cb</a> &ndash; a complete archive of the ChatBox at Informatik-forum.at (not publicly accessible; send a mail to the address specified in the footer if you desire access)</li>
 		<li><a href="//irc.rueckgr.at/archive">irc.rueckgr.at/archive</a> &ndash; an archive of some of the channels at the IRC server irc.rueckgr.at (also not publicly accessible)</li>
 		<li><a href="/wienerlinien">rueckgr.at/wienerlinien</a> &ndash; some pages (in German) based on the <a href="https://open.wien.at/site/">Open Data</a> provided by the <a href="http://www.wien.gv.at/english/">City of Vienna</a> and <a href="http://www.wienerlinien.at/eportal/ep/home.do?tabId=0">Wiener Linien</a>.</li>
-		<li><a href="//isttissdown.rueckgr.at">isttissdown.rueckgr.at</a> &ndash; a page showing whether <a href="https://tiss.tuwien.ac.at/">TISS</a> (Information Systems and Services of the <a href="http://www.tuwien.ac.at/en/tuwien_home/">Vienna University of Technology</a>); the current state is determined using <a href="https://www.icinga.org/">Icinga</a>.
-		<li><a href="//isttuweldown.rueckgr.at">isttuweldown.rueckgr.at</a> &ndash; a page showing whether <a href="https://tuwel.tuwien.ac.at/">TUWEL</a> (the e-learning platform of the <a href="http://www.tuwien.ac.at/en/tuwien_home/">Vienna University of Technology</a>); the current state is determined using <a href="https://www.icinga.org/">Icinga</a>.
+		<li><a href="//isttissdown.rueckgr.at">isttissdown.rueckgr.at</a> &ndash; a page showing whether <a href="https://tiss.tuwien.ac.at/">TISS</a> (Information Systems and Services of the <a href="http://www.tuwien.ac.at/en/tu-wien/">TU Wien</a>); the current state is determined using <a href="https://www.icinga.org/">Icinga</a>.
+		<li><a href="//isttuweldown.rueckgr.at">isttuweldown.rueckgr.at</a> &ndash; a page showing whether <a href="https://tuwel.tuwien.ac.at/">TUWEL</a> (the e-learning platform of the <a href="http://www.tuwien.ac.at/en/tu-wien/">TU Wien</a>); the current state is determined using <a href="https://www.icinga.org/">Icinga</a>.
 		<li><a href="//staroch.name">staroch.name</a> &ndash; Personal page of the server operator</li>
 		<li><a href="//cloud.rueckgr.at">cloud.rueckgr.at</a> &ndash; An instance of <a href="http://nextcloud.com/">Nextcloud</a></li>
 		<li><a href="//irc.rueckgr.at/about">chat.rueckgr.at</a> &ndash; An instance of <a href="https://rocket.chat">Rocket.Chat</a></li>
+		<li><a href="//mtb-projekt.at/">MTB-Projekt</a> &ndash; collection of course transcripts and exams from the Computer Science studies at <a href="http://www.tuwien.ac.at/en/tu-wien/">TU Wien</a></li>
 	</ul>
 	<hr />
 
